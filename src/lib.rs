3060555863e86b9afdf7e8630dd0a28d2d4dda83
Anastasiia Bulatkina <anstbulatkina@gmail.com>
2024-04-07 18:16:14 +0200

    The functions are implemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,32 @@
+use std::option::Option;
 #[derive(Debug)]
-pub struct ChessPosition;
+pub struct ChessPosition{
+    rank: i32, // row
+    file: i32, // column
+}
 
 #[derive(Debug)]
-pub struct Queen;
+pub struct Queen(ChessPosition);
 
 impl ChessPosition {
-    pub fn new(rank: i32, file: i32) -> Option<Self> {
-        unimplemented!(
-            "Construct a ChessPosition struct, given the following rank, file: ({rank}, {file}). If the position is invalid return None."
-        );
+    pub fn new(rank: i32, file: i32) -> Option<Self> { // new creates a new ChessPosition
+        // if the rank and file are between 0 and 7, we return Some(ChessPosition); if not, we return None
+        match (rank, file) { // match allows us to compare a value against a series of patterns
+            (0..=7, 0..=7) => Some(ChessPosition { rank, file }),
+            _ => None,
+        }
     }
 }
 
 impl Queen {
     pub fn new(position: ChessPosition) -> Self {
-        unimplemented!("Given the chess position {position:?}, construct a Queen struct.");
+        Queen(position) // we create a new Queen with the position
     }
 
     pub fn can_attack(&self, other: &Queen) -> bool {
-        unimplemented!("Determine if this Queen can attack the other Queen {other:?}");
+        self.0.rank == other.0.rank
+            || self.0.file == other.0.file
+            || (self.0.rank - other.0.rank).abs() == (self.0.file - other.0.file).abs()
     }
 }
+
